9f3fcd169b38fe14316e4eda101b84cb305a6af5
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-03-20 14:42:00 -0700

    added basic template and gulp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { PhotoUploadComponent } from './photoUpload.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoUploadComponent
   ],
   imports: [
     BrowserModule,
